c2ba2ffc3311a520e2723dacea2caf5f7baa95f8
SukrutaN <156003785+SukrutaN@users.noreply.github.com>
2026-02-26 21:06:36 +0530

    updated parser.js for automatic download

--- parser.js
+++ parser.js
@@ -1,17 +1,18 @@
+const results = [];
 const nodeList = document.querySelectorAll('tr');
 for(let i = 0; i < nodeList.length; i++)
 {
     const row = nodeList[i];
-    if(row.querySelector('td[data-column-index = "1"]') != null)
+    if(row.querySelector('td[data-column-index="1"]') != null)
     {   
-        const title = row.querySelector('td[data-column-index="1"]').textContent;
-        console.log(title);
-    }
-
-    if(row.querySelector('td[data-column-index = "2"]') != null)
-    {   
+        const title = row.querySelector('td[data-column-index="1"]').textContent.trim();
         const description = row.querySelector('td[data-column-index="2"]').textContent.trim();
-        console.log(description);
+        results.push({ title, description });
     }
-
 }
+
+const blob = new Blob([JSON.stringify(results, null, 2)], {type: 'application/json'});
+const a = document.createElement('a');
+a.href = URL.createObjectURL(blob);
+a.download = 'data.json';
+a.click();
